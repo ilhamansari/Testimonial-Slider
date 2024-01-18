797f6eba8e55c8299a0b340ed5598625a1beacfe
Md Ilham Ansari <ansariilham5@gmail.com>
2024-01-18 23:29:10 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import Ilham from './assets/ilham.jpg'
+import Zaid from './assets/zaid.jpg'
+import Tausif from './assets/tausif.jpg'
+import Yaseer from './assets/yasir.jpg'
+import Inaque from './assets/inaque.jpg'
+
+const reviews = [
+    {
+        id: 1,
+        name: "Ilham Ansari",
+        job: "Software Engineer",
+        image: Ilham,
+        text: "I am glad to be a software engineer. Finally, 4 years of hard work and intense coding paid off, and now I am a SDE at Amazon. I look forward to switching very soon to Google."
+    },
+    {
+        id: 2,
+        name: "Zaid Siddiqui",
+        job: "React Developer",
+        image: {Zaid},
+        text: "I am glad to be a software engineer. Finally, 4 years of hard work and intense coding paid off, and now I am a SDE at Amazon. I look forward to switching very soon to Google."
+    },
+    {
+        id: 3,
+        name: "Inaque Alam",
+        job: "Portfolio Manager",
+        image: {Inaque},
+        text: "I am glad to be a software engineer. Finally, 4 years of hard work and intense coding paid off, and now I am a SDE at Amazon. I look forward to switching very soon to Google."
+    },
+    {
+        id: 4,
+        name: "Yaseer Hassan",
+        job: "Data Analyst",
+        image: {Yaseer},
+        text: "I am glad to be a software engineer. Finally, 4 years of hard work and intense coding paid off, and now I am a SDE at Amazon. I look forward to switching very soon to Google."
+    },
+    {
+        id: 5,
+        name: "Tausif Hussain",
+        job: "HTML Developer",
+        image: {Tausif},
+        text: "I am glad to be a software engineer. Finally, 4 years of hard work and intense coding paid off, and now I am a SDE at Amazon. I look forward to switching very soon to Google."
+    }
+]
+
+export default reviews
